3e234a58ffadd8bbbfeed3ed930412f24bbb2770
nookworth <nooksack@gmail.com>
2022-09-19 18:24:10 -0600

    Inquirer prompts in functions, Jest test working.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,13 +1,18 @@
-const { it } = require("node:test");
-const { describe } = require("yargs");
 const Employee = require("../lib/employee");
 
-describe("Employee", () => {
-  describe("Initialization", () => {
-    it("Should have no properties when created with no parameters", () => {
-      const employee = new Employee();
+// describe("Employee", () => {
+//   describe("Initialization", () => {
+//     it("Should have no properties when created with no parameters", () => {
+//       const employee = new Employee();
 
-      expect("name", "id", "email" in employee).toEqual(undefined);
-    });
-  });
+//       expect("name", "id", "email" in employee).toEqual(undefined);
+//     });
+//   });
+// });
+test("On initialization, parameters should be undefined", () => {
+  const employee = new Employee();
+
+  expect(employee.name).toEqual(undefined);
+  expect(employee.id).toEqual(undefined);
+  expect(employee.email).toEqual(undefined);
 });
